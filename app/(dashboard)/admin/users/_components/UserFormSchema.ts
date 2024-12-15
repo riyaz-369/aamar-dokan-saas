1699ca8e2f1705100e76f4de3090b40d519f213d
Manishankar Vakta <manishankarvakta@Manishankars-MacBook-Pro.local>
2024-12-15 22:55:34 +0600

    update File import

--- app/(dashboard)/admin/users/_components/UserFormSchema.ts
+++ app/(dashboard)/admin/users/_components/UserFormSchema.ts
@@ -15,11 +15,11 @@ export const UserFormSchema = z.object({
   username: z.string().min(1, { message: "Username is required" }),
   type: UserType,
   permission: z.any().optional(),
-  photo: z
-    .instanceof(File)
-    .optional()
-    .or(z.null())
-    .refine((file) => !file || file.size <= 5 * 1024 * 1024, {
-      message: "Photo size must be less than 5MB",
-    }),
+  // photo: z
+  //   .instanceof(File)
+  //   .optional()
+  //   .or(z.null())
+  //   .refine((file) => !file || file.size <= 5 * 1024 * 1024, {
+  //     message: "Photo size must be less than 5MB",
+  //   }),
 });
