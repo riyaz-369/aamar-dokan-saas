09f137cd101b08ee6608cbce0bf446a68dabe11b
Manishankar Vakta <manishankarvakta@Manishankars-MacBook-Pro.local>
2024-12-15 22:46:09 +0600

    update File import

--- app/(dashboard)/admin/users/_components/UserForm.tsx
+++ app/(dashboard)/admin/users/_components/UserForm.tsx
@@ -4,6 +4,7 @@ import { Input } from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { Button } from "@/components/ui/button";
+import { File } from "node:node:buffer";
 import {
   Form,
   FormControl,
