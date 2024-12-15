6d4497cfd80c36ffc815dac826b69b25d96301e4
riyaz-369 <riyazulislam0099@gmail.com>
2024-12-15 19:29:44 +0600

    service form created

---
+++ app/(dashboard)/admin/services/_components/CreateServiceFormSchema.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+export const CreateServiceFormSchema = z.object({
+  title: z.string().min(1, "Title is required"),
+  slug: z.string().min(1, "Slug is required"),
+  code: z.string().min(1, "Code is required"),
+  categoryId: z.string(),
+  meta: z.record(z.any()).optional(),
+  packages: z.any().optional(),
+  // photo: z.string().url("Must be a valid URL").optional(),
+  description: z.string().min(1, "Description is required"),
+  tos: z.string().min(1, "Terms of service are required"),
+  privacyPolicy: z.string().min(1, "Privacy policy is required"),
+  status: z.enum(["Active", "Inactive"]).default("Active"),
+});
--- app/(dashboard)/admin/services/create/page.tsx
+++ app/(dashboard)/admin/services/create/page.tsx
@@ -1,12 +1,12 @@
 import PageTitle from "@/components/PageTitle";
 import React from "react";
-import CreateServiceForm from "./_components/CreateServiceForm";
+import CreateServiceForm from "../_components/CreateServiceForm";
 
 const CreateNewServicePage = () => {
   return (
     <div className="">
       <PageTitle title="Create New Service" />
-      <CreateServiceForm />
+      <CreateServiceForm entry={{}} />
     </div>
   );
 };
