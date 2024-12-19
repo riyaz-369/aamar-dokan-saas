7fd659fd5e27ec1c1ac1928377511566e43c401b
riyaz-369 <riyazulislam0099@gmail.com>
2024-12-19 12:05:54 +0600

    order cart page create

---
+++ app/(dashboard)/client/(order)/cart/_components/OrderCart.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const OrderCart = () => {
+  return (
+    <div>
+      <div>
+        <h3 className="text-xl font-semibold">Basic Plan</h3>
+      </div>
+    </div>
+  );
+};
+
+export default OrderCart;
--- app/(dashboard)/client/(order)/cart/page.tsx
+++ app/(dashboard)/client/(order)/cart/page.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import OrderSummary from "./_components/OrderSummary";
+import OrderCart from "./_components/OrderCart";
 
 const OrderCartPage = () => {
   return (
-    <div className="flex justify-end">
+    <div className="flex justify-between">
+      <OrderCart />
       <OrderSummary />
     </div>
   );
